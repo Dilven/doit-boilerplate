8df46560ed0cbc020786a79c5dea00833f97d39a
Dilven <mateusiakkrystian@gmail.com>
2021-11-30 19:07:57 +0100

    Add react query dev tools

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app'
 import React from 'react'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { defaultOptions } from '@/config/cache'
+import { ReactQueryDevtools } from 'react-query/devtools'
 
 const queryClient = new QueryClient({ defaultOptions });
 
@@ -10,6 +11,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   )
 }
